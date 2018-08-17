58e3fccd6bd3f1b7d7ef6e60e8b7b5356943369c
Guido Rombola <guidorombola@gmail.com>
2018-08-17 20:09:31 -0300

    Cambio indentacion tab por 4 espacios

--- fibo/src/main/java/ar/edu/untref/dyasc/Fibonacci.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Fibonacci.java
@@ -1,28 +1,28 @@
 package ar.edu.untref.dyasc;
 
 public class Fibonacci {
-	public static void main(String[] args) {
-		Fibonacci f = new Fibonacci();
-		int num = Integer.parseInt(args[0]);
-		int serie[] = f.desarrollarSerie(num);
-		String salida = "fibo<"+num+">:";
-		for(int i=0; i<serie.length; i++) {
-			salida+=" "+serie[i];
-		}
-		System.out.println(salida);
-	}
-	
-	public int[] desarrollarSerie(int n) {
-		int[] serie = new int[n];
-		if(n==1) {
-			serie[0] = 0;			
-		}
-		if(n>1) {
-			serie[1] = 1;
-		}
-		for(int i = 2; i<serie.length; i++) {
-			serie[i] = serie[i-1] + serie[i-2];
-		}
-		return serie;
-	}
+    public static void main(String[] args) {
+        Fibonacci f = new Fibonacci();
+        int num = Integer.parseInt(args[0]);
+        int serie[] = f.desarrollarSerie(num);
+        String salida = "fibo<" + num + ">:";
+        for (int i = 0; i < serie.length; i++) {
+            salida += " " + serie[i];
+        }
+        System.out.println(salida);
+    }
+
+    public int[] desarrollarSerie(int n) {
+        int[] serie = new int[n];
+        if (n == 1) {
+            serie[0] = 0;
+        }
+        if (n > 1) {
+            serie[1] = 1;
+        }
+        for (int i = 2; i < serie.length; i++) {
+            serie[i] = serie[i - 1] + serie[i - 2];
+        }
+        return serie;
+    }
 }
