fb328f664f3991aa2e35fe408408da83d5abc1cb
Guido Rombola <guidorombola@gmail.com>
2018-08-21 11:07:18 -0300

    Cambio nombre variable num a cantidadTerminos

--- fibo/src/main/java/ar/edu/untref/dyasc/MainFibonacci.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/MainFibonacci.java
@@ -3,8 +3,8 @@ package ar.edu.untref.dyasc;
 public class MainFibonacci {
     public static void main(String[] args) {
         Fibonacci f = new Fibonacci();
-        int num = Integer.parseInt(args[0]);
-        int serie[] = f.desarrollarSerie(num);
-        System.out.println(f.mostrarSerie(serie, num));
+        int cantidadTerminos = Integer.parseInt(args[0]);
+        int serie[] = f.desarrollarSerie(cantidadTerminos);
+        System.out.println(f.mostrarSerie(serie, cantidadTerminos));
     }
 }
