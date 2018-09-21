1888040ba8f2a68c1c143fa622df642fb8946463
Guido Rombola <guidorombola@gmail.com>
2018-09-20 23:03:18 -0300

    Agregados metodos de formateo de mensaje y parseo de entrada

--- bitacora/src/main/java/ar/edu/untref/dyasc/Bitacora.java
+++ bitacora/src/main/java/ar/edu/untref/dyasc/Bitacora.java
@@ -1,32 +1,46 @@
 package ar.edu.untref.dyasc;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Bitacora {
 
+    private FabricaDeSalidas creadorDeSalidas;
+
+    public Bitacora() {
+        this.creadorDeSalidas = new FabricaDeSalidas();
+    }
+
     public void registrarEvento(String mensaje) {
-        LocalDateTime fecha = LocalDateTime.now();
-        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
-        String fechaFormateada = fecha.format(formateador);
-        String salida = fechaFormateada + " " + mensaje;
-        String archivoDestino = System.getProperty("DESTINO_BITACORA");
-        if (archivoDestino == null) {
-            System.out.println(salida);
-        } else {
-            escribirEnArchivo(salida, archivoDestino);
+        String mensajeDeSalida = this.formatearMensaje(mensaje);
+        String configuracionDeSalida = System.getProperty("bitacora.destino");
+        List<Salida> salidasEvento = this.parsearEntrada(configuracionDeSalida);
+        Iterator<Salida> iterador = salidasEvento.iterator();
+        while (iterador.hasNext()) {
+            Salida salida = iterador.next();
+            salida.mostrar(mensajeDeSalida);
         }
     }
 
-    public void escribirEnArchivo(String mensaje, String archivo) {
-        try {
-            FileWriter escritorArchivoSalida = new FileWriter(archivo, true);
-            escritorArchivoSalida.write(mensaje + "\n");
-            escritorArchivoSalida.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private List<Salida> parsearEntrada(String parametros) {
+        StringTokenizer tokenizer = new StringTokenizer(parametros, ",");
+        List<Salida> salidasEvento = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()) {
+            String parametroDeSalida = tokenizer.nextToken();
+            salidasEvento.add(creadorDeSalidas.generar(parametroDeSalida));
         }
+        return salidasEvento;
+    }
+
+    private String formatearMensaje(String mensaje) {
+        LocalDateTime fecha = LocalDateTime.now();
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        String fechaFormateada = fecha.format(formateador);
+        String mensajeDeSalida = fechaFormateada + " " + mensaje;
+        return mensajeDeSalida;
     }
 }
