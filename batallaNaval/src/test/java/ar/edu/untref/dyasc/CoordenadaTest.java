92339ad6140004edfcf591c721c89276a4770e2a
Guido Rombola <guidorombola@gmail.com>
2018-09-29 20:49:40 -0300

    Definida y testeada la igualdad entre dos coordenadas

--- batallaNaval/src/test/java/ar/edu/untref/dyasc/CoordenadaTest.java
+++ batallaNaval/src/test/java/ar/edu/untref/dyasc/CoordenadaTest.java
@@ -13,5 +13,29 @@ public class CoordenadaTest {
         
         assertEquals(coordenada1, coordenada2);
     }
+    
+    @Test
+    public void dosCoordenadasConElMismoXPeroDistintoYDebenSerDiferentes() {
+        Coordenada coordenada1 = new Coordenada(3,3);
+        Coordenada coordenada2 = new Coordenada(3,4);
+        
+        assertNotEquals(coordenada1, coordenada2);
+    }
+    
+    @Test
+    public void dosCoordenadasConElMismoYPeroDistintoXDebenSerDiferentes() {
+        Coordenada coordenada1 = new Coordenada(3,3);
+        Coordenada coordenada2 = new Coordenada(4,3);
+        
+        assertNotEquals(coordenada1, coordenada2);
+    }
+    
+    @Test
+    public void dosCoordenadasConDisntoXeYDebenSerDiferentes() {
+        Coordenada coordenada1 = new Coordenada(3,3);
+        Coordenada coordenada2 = new Coordenada(4,2);
+        
+        assertNotEquals(coordenada1, coordenada2);
+    }
 
 }
