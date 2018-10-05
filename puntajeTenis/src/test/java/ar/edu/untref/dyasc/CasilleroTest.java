0fae4820892e3c83c8c450d3cd2822e9bb37954c
Guido Rombola <guidorombola@gmail.com>
2018-10-05 19:03:16 -0300

    Inicio tarea

---
+++ puntajeTenis/src/test/java/ar/edu/untref/dyasc/CasilleroTest.java
@@ -0,0 +1,49 @@
+package ar.edu.untref.dyasc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CasilleroTest {
+
+    @Test
+    public void dosCasillerosConElMismoXeYDebenSerIguales() {
+        Casillero casillero1 = new Casillero(3, 3);
+        Casillero casillero2 = new Casillero(3, 3);
+
+        assertEquals(casillero1, casillero2);
+    }
+
+    @Test
+    public void dosCasillerosConElMismoXPeroDistintoYDebenSerDiferentes() {
+        Casillero casillero1 = new Casillero(3, 3);
+        Casillero casillero2 = new Casillero(3, 4);
+
+        assertNotEquals(casillero1, casillero2);
+    }
+
+    @Test
+    public void dosCasillerosConElMismoYPeroDistintoXDebenSerDiferentes() {
+        Casillero casillero1 = new Casillero(3, 3);
+        Casillero casillero2 = new Casillero(4, 3);
+
+        assertNotEquals(casillero1, casillero2);
+    }
+
+    @Test
+    public void dosCasillerosConDisntoXeYDebenSerDiferentes() {
+        Casillero casillero1 = new Casillero(3, 3);
+        Casillero casillero2 = new Casillero(4, 2);
+
+        assertNotEquals(casillero1, casillero2);
+    }
+
+    @Test
+    public void dosCasillerosConLosMismosXeYTienenElMismoHashCode() {
+        Casillero casillero1 = new Casillero(3, 3);
+        Casillero casillero2 = new Casillero(3, 3);
+
+        assertEquals(casillero1.hashCode(), casillero2.hashCode());
+    }
+
+}
