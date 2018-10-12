a0c6d8ac15573fe54e57a957d88b32510de21d7f
Guido Rombola <guidorombola@gmail.com>
2018-10-12 12:19:23 -0300

    Agregado mensaje a ExcepcionLimitesInvalidos

--- batallaNaval/src/main/java/ar/edu/untref/dyasc/ExcepcionLimitesInvalidos.java
+++ batallaNaval/src/main/java/ar/edu/untref/dyasc/ExcepcionLimitesInvalidos.java
@@ -2,4 +2,7 @@ package ar.edu.untref.dyasc;
 
 public class ExcepcionLimitesInvalidos extends Exception {
 
+    public ExcepcionLimitesInvalidos() {
+        super("El barco que desea aniadir no esta comprendido dentro de los limites validos del tablero");
+    }
 }
